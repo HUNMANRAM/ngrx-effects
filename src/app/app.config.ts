c3e3edeb092eee8f10854e1a3ec111096c82f1e6
HUNMANRAM <hunmanram143@gmail.com>
2024-12-04 21:49:27 +0530

    some changes;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,43 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
+//import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { userEffects } from './user.effects';
 import { userReducer } from './user.reducer'; 
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ShowTodosComponent } from './show-todos/show-todos.component';
+
+const routes = [
+  { path: 'add-todo', component: AddTodoComponent },
+  { path: 'show-todos', component: ShowTodosComponent },
+  { path: '', redirectTo: '/show-todos', pathMatch: 'full' } // Default route
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withComponentInputBinding()),
     provideHttpClient(), provideStore({user:userReducer}), provideEffects([userEffects])]
 };
 
+
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter, withComponentInputBinding } from '@angular/router';
+// import { AddTodoComponent } from './add-todo/add-todo.component';
+// import { ShowTodosComponent } from './show-todos/show-todos.component';
+// import { provideLocationStrategy, PathLocationStrategy } from '@angular/common';
+
+// const routes = [
+//   { path: 'add-todo', component: AddTodoComponent },
+//   { path: 'show-todos', component: ShowTodosComponent },
+//   { path: '', redirectTo: '/show-todos', pathMatch: 'full' } // Default route
+// ];
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes, withComponentInputBinding()),
+//     provideLocationStrategy(PathLocationStrategy)
+//   ]
+// };
---
+++ src/app/show-todos/show-todos.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-todos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './show-todos.component.html',
+  styleUrl: './show-todos.component.css'
+})
+export class ShowTodosComponent {
+
+}
